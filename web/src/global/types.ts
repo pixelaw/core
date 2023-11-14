c877ee554ce3924d1de6afc1c46c0ae3c03477c7
mariz <marrise@oostvoort.com>
2023-11-14 10:32:55 +0800

    refactor: remove unnecessary files

--- web/src/global/types.ts
+++ web/src/global/types.ts
@@ -1,59 +1,5 @@
 import React from 'react'
 
-import { PAPER, ROCK, SCISSORS } from './constants'
-
-export type OptionType = typeof ROCK | typeof PAPER | typeof SCISSORS
-
-export type OwnerComponent = {
-  address: string,
-  __typename: "Owner"
-}
-
-export type PositionComponent = {
-  x: number,
-  y: number,
-  __typename: "Position"
-}
-
-export type PixelTypeComponent = {
-  name: string,
-  __typename: "PixelType"
-}
-
-export type TimestampComponent = {
-  created_at: number
-  updated_at: number
-  __typename: "Timestamp"
-}
-
-export type ColorComponent = {
-  r: number
-  g: number
-  b: number
-  __typename: "Color"
-}
-
-export type ColorCountComponent = {
-  count: string
-  __typename: "ColorCount"
-}
-
-
-export type PixelEntity = {
-  id: string,
-  owner?: string,
-  position?: { x: number, y: number },
-  pixelType?: string,
-  createdAt?: number,
-  updatedAt?: number,
-  color?: {
-    r: number,
-    g: number,
-    b: number
-  },
-  colorCount?: number
-}
-
 export enum Active_Page {
   Home,
   Network,
