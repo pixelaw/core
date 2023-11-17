7ab16aaecea39054d0cd9847e5eb53926a7f79d1
mariz <marrise@oostvoort.com>
2023-11-17 13:17:55 +0800

    feat: add toast

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import MainLayout from "@/components/layouts/MainLayout";
 import ScreenAtomRenderer from "@/components/ScreenAtomRenderer";
+import { Toaster } from '@/components/ui/toaster'
 
 function App() {
   return (
       <MainLayout>
           <ScreenAtomRenderer/>
+          <Toaster />
       </MainLayout>
   );
 }
