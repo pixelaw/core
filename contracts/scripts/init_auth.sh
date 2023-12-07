e3b4a33782327bfaa48d543c63a8b2f919c3b54f
mariz <marrise@oostvoort.com>
2023-12-07 15:24:47 +0800

    chore: get rpc url from param

--- contracts/scripts/init_auth.sh
+++ contracts/scripts/init_auth.sh
@@ -8,6 +8,15 @@ cp target/dev/manifest.json target/dev/manifest.copy.json
 
 source scripts/update_contracts.sh
 
+# Set RPC_URL with default value
+RPC_URL="http://localhost:5050"
+
+# Check if a command line argument is supplied
+if [ $# -gt 0 ]; then
+    # If an argument is supplied, use it as the RPC_URL
+    RPC_URL=$1
+fi
+
 # make sure all components/systems are deployed
 CORE_MODELS=("App" "AppName" "CoreActionsAddress" "Pixel" "Permissions" "QueueItem")
 
@@ -16,29 +25,29 @@ SNAKE_MODELS=("Snake" "SnakeSegment")
 echo "Write permissions for CORE_ACTIONS"
 for model in ${CORE_MODELS[@]}; do
     sleep 0.1
-    sozo auth writer $model $CORE_ACTIONS
+    sozo auth writer $model $CORE_ACTIONS --rpc-url RPC_URL
 done
 echo "Write permissions for CORE_ACTIONS: Done"
 
 echo "Write permissions for SNAKE_ACTIONS"
 for model in ${SNAKE_MODELS[@]}; do
     sleep 0.1
-    sozo auth writer $model $SNAKE_ACTIONS
+    sozo auth writer $model $SNAKE_ACTIONS --rpc-url RPC_URL
 done
 echo "Write permissions for SNAKE_ACTIONS: Done"
 
 
 echo "Initialize CORE_ACTIONS : $CORE_ACTIONS"
 sleep 0.1
-sozo execute $CORE_ACTIONS init
+sozo execute $CORE_ACTIONS init --rpc-url RPC_URL
 echo "Initialize CORE_ACTIONS: Done"
 
 echo "Initialize SNAKE_ACTIONS: Done"
 sleep 0.1
-sozo execute $SNAKE_ACTIONS init
+sozo execute $SNAKE_ACTIONS init --rpc-url RPC_URL
 echo "Initialize SNAKE_ACTIONS: Done"
 
 echo "Initialize PAINT_ACTIONS: Done"
 sleep 0.1
-sozo execute $PAINT_ACTIONS init
+sozo execute $PAINT_ACTIONS init --rpc-url RPC_URL
 echo "Initialize PAINT_ACTIONS: Done"
