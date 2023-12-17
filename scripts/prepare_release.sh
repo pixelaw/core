098334d9544a4e2365325ea76ce8f912ded81e9a
Caspar Oostendorp <caspar@oostvoort.com>
2023-12-18 06:54:24 +0800

    adding release script and version

---
+++ scripts/prepare_release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -euxo pipefail
+
+if [ ! -f VERSION ]; then
+    echo "VERSION file does not exist"
+    exit 1
+fi
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+prev_version=$(cat VERSION)
+next_version=$1
+
+find . -type f -name "*.toml" -exec sed -i'' -e "s/version = \"$prev_version\"/version = \"$next_version\"/g" {} \;
+find . -type f -name "Makefile" -exec sed -i'' -e "s/CORE_VERSION = $prev_version/CORE_VERSION = $next_version/g" {} \;
+find . -type f -name "package.json" -exec sed -i'' -e "s/\"version\": \"$prev_version\"/\"version\": \"$next_version\"/g" {} \;
+
+echo $1 > VERSION
+
+# Uncommented git commands
+git commit -am "Prepare v$1"
+git tag -a "v$1" -m "Version $1"
+
