5b85d0e4cc6229d01d106a4fc4a033099f698464
Caspar Oostendorp <caspar@oostvoort.com>
2024-05-08 04:24:00 +0800

    removing /web .. its now at https://github.com/pixelaw/web

--- web/src/hooks/utils/useAccountAddress.ts
+++
@@ -1,9 +0,0 @@
-import { useQueryClient } from '@tanstack/react-query'
-import { getUrlParam } from '@/global/utils'
-
-const useAccountAddress = () => {
-  const queryClient = useQueryClient();
-  return queryClient.getQueryData<string>(['createAccount', getUrlParam('account', 0)])
-}
-
-export default useAccountAddress
