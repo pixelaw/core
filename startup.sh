cdb67e7024697a505a8ed9904de00cd9ec737dd3
Caspar Oostendorp <caspar@oostvoort.com>
2024-05-09 06:03:38 +0800

    no longer do vite env importing on startup. will replace this with a template config json

--- startup.sh
+++ startup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-function buildWebApp() {
-  npx import-meta-env -x .env.core -p static/index.html
-}
-
 # Define the first function (thread)
 function thread1() {
     cd bots && npm run dev > ../log/bots.log 2>&1;
@@ -14,7 +10,6 @@ function thread2() {
    ./keiko
 }
 
-buildWebApp
 
 # Start the first thread in the background
 thread1 &
