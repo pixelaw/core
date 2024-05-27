806f551944b753f8586d82227604b84dd26d9296
Caspar Oostendorp <caspar@oostvoort.com>
2024-05-28 05:33:24 +0800

    fixing deployment

--- docker/scripts/torii_start.sh
+++ docker/scripts/torii_start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 source /root/.bashrc
 
+# Wait for Katana to start
+while ! netstat -tuln | grep ':5050 ' > /dev/null; do
+  sleep 1
+done
+
 torii \
   --rpc "http://0.0.0.0:5050" \
   --world $WORLD_ADDRESS \
