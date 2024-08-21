5108d92fe18eb9d0536f0506f7dce7691aefe306
Asuma Yamada <yamadaasuma@gmail.com>
2024-08-20 21:02:07 -0400

    tweak

--- contracts/scripts/init_auth.sh
+++ contracts/scripts/init_auth.sh
@@ -2,8 +2,9 @@
 set -euo pipefail
 pushd $(dirname "$0")/..
 
-export TARGET=${1:-"manifests/dev"}
+export TARGET=${1:-"manifests/dev/deployment"}
 export STARKNET_RPC="http://localhost:5050/"
+
 
 GENESIS_TEMPLATE=genesis_template.json
 GENESIS_OUT=genesis.json
@@ -26,23 +27,28 @@ if [ $# -gt 0 ]; then
 fi
 
 # make sure all components/systems are deployed
-CORE_MODELS=("App" "AppName" "CoreActionsAddress" "Pixel" "Permissions" "QueueItem")
+CORE_MODELS=("pixelaw-App" "pixelaw-AppName" "pixelaw-CoreActionsAddress" "pixelaw-Pixel" "pixelaw-Permissions" "pixelaw-QueueItem" "pixelaw-Snake" "pixelaw-Instruction")
 
-SNAKE_MODELS=("Snake" "SnakeSegment")
+SNAKE_MODELS=("pixelaw-Snake" "pixelaw-SnakeSegment")
 
 echo "Write permissions for CORE_ACTIONS"
 for model in ${CORE_MODELS[@]}; do
     sleep 0.1
-    sozo  --profile $SCARB_PROFILE auth grant writer $model,$CORE_ACTIONS
+    sozo  --profile $SCARB_PROFILE auth grant writer model:$model,$CORE_ACTIONS
 done
 echo "Write permissions for CORE_ACTIONS: Done"
 
 echo "Write permissions for SNAKE_ACTIONS"
 for model in ${SNAKE_MODELS[@]}; do
     sleep 0.1
-    sozo --profile $SCARB_PROFILE auth grant writer  $model,$SNAKE_ACTIONS
+    sozo --profile $SCARB_PROFILE auth grant writer model:$model,$SNAKE_ACTIONS
 done
 echo "Write permissions for SNAKE_ACTIONS: Done"
+
+echo "Write permissions for PAINT_ACTIONS"
+sleep 0.1
+sozo --profile $SCARB_PROFILE auth grant writer model:pixelaw-Pixel,$PAINT_ACTIONS
+echo "Write permissions for PAINT_ACTIONS: Done"
 
 
 echo "Initialize CORE_ACTIONS : $CORE_ACTIONS"
