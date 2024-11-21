b190f5521cf27d524c1bee1cd6ac7a3bb22b204b
thiscaspar <thiscaspar@gmail.com>
2024-11-21 08:49:45 +0800

    fix hosting

--- docker/scripts/torii_start.sh
+++ docker/scripts/torii_start.sh
@@ -2,12 +2,16 @@
 source /root/.bashrc
 
 # Wait for Katana to start
-while ! netstat -tuln | grep ':5050 ' > /dev/null; do
-  sleep 1
-done
+# TODO: this is not playing nice with katana not on the same server.
+if [ ! "$DISABLE_KATANA" == "1" ]; then
+  while ! netstat -tuln | grep ':5050 ' > /dev/null; do
+    sleep 1
+  done
+fi
 
 torii \
-  --rpc "http://0.0.0.0:5050" \
+  --rpc $RPC_URL \
   --world $WORLD_ADDRESS \
   --http.cors_origins "*" \
-  --db-dir $TORII_DB
+  --db-dir $TORII_DB \
+  --http.addr 0.0.0.0
