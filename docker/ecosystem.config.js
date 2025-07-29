0a847f09bac7d75a90e6969e8a289f485f5e100c
thiscaspar <thiscaspar@gmail.com>
2025-07-30 03:01:14 +0800

    Update docker-compose and PM2 config for pixelaw-core and logging
    - Replace keiko service with pixelaw-core in docker-compose.yml
    - Add environment variables and adjust volumes for pixelaw-core
    - Redirect PM2 app logs to stdout/stderr for container logging
      integration

--- docker/ecosystem.config.js
+++ docker/ecosystem.config.js
@@ -2,22 +2,22 @@ module.exports = {
   apps : [{
     name: 'katana',
     script: '/pixelaw/scripts/katana_start.sh',
-    out_file: '/pixelaw/log/katana.log',
-    error_file: '/pixelaw/log/katana.log',
+    out_file: '/proc/1/fd/1',
+    error_file: '/proc/1/fd/2',
     merge_logs: true,
     time: true
   },{
     name: 'torii',
     script: '/pixelaw/scripts/torii_start.sh',
-    out_file: '/pixelaw/log/torii.log',
-    error_file: '/pixelaw/log/torii.log',
+    out_file: '/proc/1/fd/1',
+    error_file: '/proc/1/fd/2',
     merge_logs: true,
     time: true
   },{
     name: 'server',
     script: '/pixelaw/scripts/server_start.sh',
-    out_file: '/pixelaw/log/server.log',
-    error_file: '/pixelaw/log/server.log',
+    out_file: '/proc/1/fd/1',
+    error_file: '/proc/1/fd/2',
     merge_logs: true,
     time: true
   }]
