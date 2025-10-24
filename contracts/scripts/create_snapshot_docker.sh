08aa0f8594b815a915ad46f8bae72173d68c1c75
OwnerOfJK <john.kaller@hotmail.de>
2025-10-24 18:55:00 -0300

    Check for manifest.json before copying

--- contracts/scripts/create_snapshot_docker.sh
+++ contracts/scripts/create_snapshot_docker.sh
@@ -18,7 +18,14 @@ STORAGE_INIT_WORLD="/pixelaw/storage_init"
 mkdir -p $STORAGE_INIT_WORLD
 
 cp $GENESIS_TEMPLATE $STORAGE_INIT_WORLD/genesis.json
-cp manifest_$PROFILE.json $STORAGE_INIT_WORLD/manifest.json
+
+# Copy manifest file (manifest_$PROFILE.json from root or target/$PROFILE/manifest.json)
+MANIFEST_FILE="manifest_$PROFILE.json"
+if [ ! -f "$MANIFEST_FILE" ]; then
+    MANIFEST_FILE="target/$PROFILE/manifest.json"
+fi
+
+cp $MANIFEST_FILE $STORAGE_INIT_WORLD/manifest.json
 
 cp $KATANA_DB_ZIP $STORAGE_INIT_WORLD/katana_db.zip
 
